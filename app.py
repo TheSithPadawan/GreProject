0578d19ba508cfad297d18db23bfeb22297f3fe5
TheSithPadawan <miaorr1994@gmail.com>
2018-05-31 23:59:46 -0400

    changed client side

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import json
 from flask import jsonify
 
 app = Flask(__name__)
-
 app.config.update(
     SECRET_KEY = 'Florence',
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Florence@localhost/gre_db',
@@ -26,24 +25,10 @@ def get_data():
 # handles client post request for answer submission
 @app.route('/action',methods = ['POST'])
 def get_message():
-    ans1 = request.form['ans1']
-    ans2 = request.form['ans2']
-    ans1 = sorted(list(ans1))
-    ans2 = sorted(list(ans2))
-    query = db.session.query(Answer).all()
-    ans_dict = {}
-    for row in query:
-        ans_dict[row.question_id] = list(row.answers)
-    if ans_dict[1] != ans1:
-        disp1 = "Your answer is incorrect. The correct answer for Q1 is {}.".format("".join(ans_dict[1]))
-    else:
-        disp1 = "Your answer to Q1 is correct."
-    if ans_dict[2] != ans2:
-        disp2 = "Your answer is incorrect. The correct answer for Q2 is {}.".format("".join(ans_dict[2]))
-    else:
-        disp2 = "Your answer to Q2 is correct."
-    response = [disp1, disp2]
-    return render_template("action.html", response = response)
+    result = request.get_json()
+    print(result['ans1'])
+    print(result['ans2'])
+    return "200"
 
 class Question(db.Model):
     __tablename__ = 'question'

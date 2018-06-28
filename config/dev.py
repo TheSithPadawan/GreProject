c9045351dafb3e52ac1ca9a9f1c0b51e0c7ecee5
5lkMdy76 <hzccx94@gmail.com>
2018-06-28 12:22:25 +0800

    temp

--- config/dev.py
+++ config/dev.py
@@ -1,5 +1,5 @@
 # dev config
 DEBUG = True
 SECRET_KEY = 'Florence'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/gre_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgresAdmin:Florence@gredb.cuql01p1pirt.us-west-1.rds.amazonaws.com/greDB'
 SQLALCHEMY_TRACK_MODIFICATIONS = False

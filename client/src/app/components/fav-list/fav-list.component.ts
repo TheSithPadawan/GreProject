a177e61ebda94a8496c9a2aa405e12263ffb17ef
WilliamShi1 <FeifanShi@Feifans-MacBook-Pro.local>
2018-07-05 23:34:20 -0400

    msg

--- client/src/app/components/fav-list/fav-list.component.ts
+++ client/src/app/components/fav-list/fav-list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FavListService} from '../../services/fav_list.service';
-import {ListItem} from './listItem';
 
 @Component({
   selector: 'app-fav-list',
@@ -8,13 +6,5 @@ import {ListItem} from './listItem';
   styleUrls: ['./fav-list.component.css']
 })
 export class FavListComponent implements OnInit {
-  listItems: ListItem[] = []
-  constructor(private favList: FavListService) { }
-
-  ngOnInit() {
-    this.favList.getFavList().subscribe(
-      (response) => console.log(response),
-      (error) => console.log(error)
-    );
-  }
+  ngOnInit(){}
 }

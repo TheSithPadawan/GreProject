930ba876451a4c03e52997aa5b568ea27294d7ee
Mingwei Chen <chenmingwei@hotmail.com>
2018-07-15 16:50:20 -0500

    Implemented history

---
+++ server/models/history.py
@@ -0,0 +1,46 @@
+from server import db
+
+class QuestionHistoryModel(db.Model):
+    __tablename__ = 'questionhistory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(20))
+    question_id = db.Column(db.String(20))
+    timestamp = db.Column(db.DateTime)
+
+    def __init__(self, user_id, question_id):
+        self.user_id = user_id
+        self.question_id = question_id
+
+    def save_to_db(self):
+        db.session.merge(self)
+        db.session.commit()
+        db.session.refresh(self)
+        return self.id
+
+    @classmethod
+    def find_by_username(cls, user_id):
+        return cls.query.filter_by(user_id=user_id).all()
+
+    @classmethod
+    def find_by_username_question(cls, user_id, question_id):
+        return cls.query.filter_by(user_id=user_id, question_id=question_id).order_by(cls.timestamp).all()
+
+class AnswerHistoryModel(db.Model):
+    __tablename__ = 'answerhistory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    history_id = db.Column(db.String(20))
+    answer_id = db.Column(db.String(20))
+
+    def __init__(self, history_id, answer_id):
+        self.history_id = history_id
+        self.answer_id = answer_id
+
+    def save_to_db(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_history(cls, history_id):
+        return cls.query.filter_by(history_id=history_id).all()
--- server/resources/answer_resource.py
+++ server/resources/answer_resource.py
@@ -1,5 +1,6 @@
 from flask_restful import Api, Resource, reqparse
 from server.models.answer import AnswerModel
+from flask_jwt_extended import jwt_required, get_jwt_identity,get_raw_jwt
 
 class Answer(Resource):
     parser = reqparse.RequestParser()
@@ -17,10 +18,27 @@ class Answer(Resource):
             return ans.json()
         return {'message':'answer to question with id {} does not exist'.format(id_)}, 404
 
+    @jwt_required
     def post(self, id_):
         ans = AnswerModel.get_answer_by_id(id_)
         data = Answer.parser.parse_args()
         ans_list = sorted([data['usr_ans1'], data['usr_ans2']])
+
+
+        current_user = get_jwt_identity()
+        if current_user is not None:
+            # save to history
+            question_history = QuestionHistoryModel(str(current_user), str(_id))
+            history_id = question_history.save_to_db()
+            usr_ans1 = AnswerHistoryModel(str(history_id), data['usr_ans1']);
+            usr_ans2 = AnswerHistoryModel(str(history_id), data['usr_ans2']);
+            usr_ans1.save_to_db();
+            usr_ans2.save_to_db();
+
+
+
+
+
         if ans.answer1 == ans_list[0] and ans.answer2 == ans_list[1]:
             return {'message': 'answer is correct'}
         return {'message': 'answer is incorrect'}

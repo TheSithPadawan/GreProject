d15254926e9bd67960ae3e0e7655cff147ee5589
TheSithPadawan <miaorr1994@gmail.com>
2018-07-22 20:35:15 -0700

    added test data for note function front end

---
+++ client/src/app/components/note/note.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {NoteService} from '../../services/note.service';
+import {Question} from '../question/question.component';
+import {Answer} from '../question/question.component';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent implements OnInit {
+  allNotes: Note[] = [];
+  constructor(private note: NoteService) { }
+
+  ngOnInit() {
+    this.note.getUserNote().subscribe(
+      (response: any[]) => {
+        // console.log(response);
+        for (let t = 0; t < response.length; t++) {
+          const oneNote = new Note();
+          const q = new Question();
+          q.id = response[t]['id'];
+          q.question = response[t]['question'];
+          // some transformation of the data
+          for (let i = 0; i < response[t]['options'].length; i++) {
+            const pair = response[t]['options'][i];
+            for (const key in pair) {
+              if (pair.hasOwnProperty(key)) {
+                q.options.push(
+                  {
+                    label: key,
+                    content: pair[key]
+                  }
+                );
+              }
+            }
+          }
+          q.usr_ans1 = response[t]['user answer'][0];
+          q.usr_ans2 = response[t]['user answer'][1];
+          oneNote.question = q;
+          oneNote.answer = new Answer();
+          oneNote.answer.answer1 = response[t]['correct answer'][0];
+          oneNote.answer.answer2 = response[t]['correct answer'][1];
+          oneNote.notes = response[t]['notes'];
+          this.allNotes.push(oneNote);
+        }
+      },
+      (error) => console.log(error),
+      () => console.log(this.allNotes)
+    );
+  }
+}
+
+export class Note {
+  question: Question;
+  notes: string;
+  answer: Answer;
+}
---
+++ client/src/app/services/note.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AuthenticationService} from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': <string> this.auth.currentToken(),
+    })
+  };
+  constructor(private http: HttpClient, private auth: AuthenticationService) {
+  }
+  getUserNote(){
+      return this.http.get('http://localhost:8152/note', this.httpOptions);
+   }
+}
